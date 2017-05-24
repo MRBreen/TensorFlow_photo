99add2f1139d614b517760c82a98f03fe1d52ee5
Mike Breen <breen.mr(at)gmail.com>
2017-05-23 18:40:20 -0700

    calculates error delta

--- core_tutorial.py
+++ core_tutorial.py
@@ -27,3 +27,13 @@ init = tf.global_variables_initializer()
 sess.run(init)
 
 print(sess.run(linear_model, {x:[1,2,3,4]}))
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+fixW = tf.assign(W, [-1.1])
+fixb = tf.assign(b, [1.1])
+sess.run([fixW, fixb])
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
