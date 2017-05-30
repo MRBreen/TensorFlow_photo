52bedec1a49c2573c934b5cc922572b1653aa4cb
Mike Breen <breen.mr(at)gmail.com>
2017-05-30 13:16:13 -0700

    renamed hosting folder

--- core_tutorial.py
+++ core_tutorial.py
@@ -1,18 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-node1 = tf.constant(3.0, tf.float32)
-node2 = tf.constant(4.0) # also tf.float32 implicitly
-print(node1, node2)
-
-sess = tf.Session()
-#a = tf.placeholder(tf.float32)
-#b = tf.placeholder(tf.float32)
-#adder_node = a + b  # + provides a shortcut for tf.add(a, b)
-
-#print(sess.run(adder_node, {a: 3, b:4.5}))
-#print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
-
+# Model parapeters
 W = tf.Variable([.3], tf.float32)
 b = tf.Variable([-.3], tf.float32)
 # Model input and output
@@ -20,20 +9,16 @@ x = tf.placeholder(tf.float32)
 linear_model = W * x + b
 y = tf.placeholder(tf.float32)
 
-#init = tf.global_variables_initializer()
-#sess.run(init)
-
-#print(sess.run(linear_model, {x:[1,2,3,4]}))
-
 #loss
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
 
 # optimizer
-optimizer = tf.train.GradientDescentOptimizer(0.0001)
+optimizer = tf.train.GradientDescentOptimizer(0.001)
 train = optimizer.minimize(loss)
 #print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
 
+# training data
 x_train = [1,2,3,4]
 y_train = [0,-1,-2,-3]
 
@@ -41,16 +26,8 @@ y_train = [0,-1,-2,-3]
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init) # reset values to incorrect defaults
-#fixW = tf.assign(W, [-1.1])
-#fixb = tf.assign(b, [1.1])
-#sess.run([fixW, fixb])
-#print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
-
-#optimizer = tf.train.GradientDescentOptimizer(0.01)
-#train = optimizer.minimize(loss)
-
 #sess.run(init) # reset values to incorrect defaults.
-for i in range(1000000):
+for i in range(10000):
   sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
 
 #evaluate training accuracy
